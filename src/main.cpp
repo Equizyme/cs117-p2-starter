2dbfe28437a6c98351b58b282f6d069b6e60d6fe
Gabe Cavazos <gabriel.r.cavazos@gmail.com>
2024-09-20 09:22:13 -0600

    Update main.cpp
    Wrote code

--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,17 @@
-//Students Write your code here!
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+  double temp;
+  double entropy;
+
+  cout << "Enter a tempurature in C (150-500):";
+  cin >> temp;
+  entropy = 7.2810 + (7.5081 - 7.2810)*((temp-150)/(200-150));
+  cout << "The entropy is: " << entropy << " kJ/(kg.k)" << endl;
+ return 0;
+   }
+
 #define UNUSED(x) (void)x
