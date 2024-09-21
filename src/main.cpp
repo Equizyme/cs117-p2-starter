27f33c1c4bc9140d993ed864ba5e770e73b1c842
Gabe Cavazos <gabriel.r.cavazos@gmail.com>
2024-09-21 13:53:04 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ int main()
   double temp;
   double entropy;
 
-  cout << "Enter a tempurature in C (150-500):";
+  cout << "Enter a temperature in C (150-500):";
   cin >> temp;
   entropy = 7.2810 + (7.5081 - 7.2810)*((temp-150)/(200-150));
   cout << "The entropy is: " << entropy << " kJ/(kg.k)" << endl;
