2791450493cc82ed2d04e9eec1b335dc130ca5e9
Gabe Cavazos <gabriel.r.cavazos@gmail.com>
2024-10-01 15:11:10 -0600

    update

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,11 @@ using namespace std;
 int main()
 {
   double temp;
-  double X0 = 0;
-  double X1 = 0;
-  double Y0 = 0;
-  double Y1 = 0;
+  const int T1 = 150;
+  const int T2 = 250;
+  const int T3 = 300;
+  const int T4 = 400;
+  const int T5 = 500;
   double entropy;
 
   cout << "Enter a temperature in C (150-500):";
@@ -18,36 +19,21 @@ int main()
   }
 
   
-  
   else {
-    if ((temp >= 150) && (temp <= 250)) {X0 = 150;
-    X1 = 250;
-    Y0 = 7.281;
-    Y1 = 7.71;
+    if ((temp >= 150) && (temp <= 250)) {entropy = (7.71 + (7.71 - 7.281)*((temp-T1)/(T2-T1)));
     }
     
-    else if ((temp > 250) && (temp <= 300)) {X0 = 250;
-    X1 = 300;
-    Y0 = 7.71;
-    Y1 = 11;
+    else if ((temp > 250) && (temp <= 300)) { entropy = (7.71 + (11 - 7.71)*((temp-T2)/(T3-T2)));
     }
    
-
-    else if ((temp >= 300) && (temp <= 400)) {X0 = 300;
-    X1 = 400;
-    Y0 = 11;
-    Y1 = 8.2236;
+    else if ((temp >= 300) && (temp <= 400)) {entropy = (11 + (8.2236 - 11)*((temp-T3)/(T4-T3)));
     }
 
-    else if ((temp > 400) && (temp <= 500)) {X0 = 400;
-    X1 = 500;
-    Y0 = 8.2236;
-    Y1 = 8.5153;
+    else if ((temp > 400) && (temp <= 500)) {entropy = (8.2236 + (8.51530 - 8.2236)*((temp-T4)/(T5-T4)));
     }
   
-  entropy = (Y0 + (Y1 - Y0)*((temp-X0)/(X1-X0)));
-  }
   cout << "The entropy is: " << entropy << " kJ/(kg.k)" << endl;
+  }
    
  return 0;
    }
